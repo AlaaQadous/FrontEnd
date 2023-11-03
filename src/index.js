429467b96cfe66783859e6a5bd03480058c65b9e
AlaaQadous <hakooooors123love@gmail.com>
2023-11-03 22:50:09 +0200

    no commit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import ProfileSettings from './customer/profil';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <SignInSide/>
+  <ProfileSettings/>
   </React.StrictMode>
 );
 
